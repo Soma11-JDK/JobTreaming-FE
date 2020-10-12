1641ce0d23530c6a8b822445cd40a976c06dd7e8
SeongJin Kim <dge03078@gmail.com>
2020-10-12 20:23:37 +0900

    [#20] 인기 강연 코드 리팩토링
    홈 화면에서 보이는 인기 강연 미리보이는 화면 코드 리팩토링
    LecturePreview 컴포너트를 사용할 수 있게 변경

--- src/Components/LecturePreview.js
+++ src/Components/LecturePreview.js
@@ -3,70 +3,161 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  font-size: 12px;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  margin-top: 10px;
-  left: 5px;
-  bottom: 10px;
-  width: calc(100% - 10px);
-  padding-top: 66.6%;
+  width: 100%;
+  height: 100%;
 `;
 
-const Image = styled.div`
-  background-image: url(${props => props.bgUrl});
-  background-repeat: no-repeat;
-  background-size: contain;
+const Heart = styled.img`
+  cursor: pointer;
+  z-index: 10;
   position: absolute;
+  width: auto;
+  height: auto;
   top: 0;
-  background-position: center center;
-  transition: opacity 0.1s linear;
-  width: 100%;
-  height: 80%;
-  margin: 5px auto;
+  margin-top: 0.5%;
+  margin-left: 1vw;
 `;
 
 const ImageContainer = styled.div`
-  margin-bottom: 30px;
-  &:hover {
-    ${Image} {
-      opacity: 0.3;
-    }
+  height: auto;
+`;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
+const InfoTopContainer = styled.div`
+  height: 30px;
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 17px;
+  align-content: center;
+
+  ${InfoTopContainer} {
+    display: flex;
   }
 `;
 
-const Title = styled.span`
-  display: block;
-  font-size: 1.8vw;
-  margin-top: 8px;
-  margin-left: 10px;
+const InfoMidContainer = styled.div`
+  display: flex;
+  height: 56px;
 `;
 
-const Date = styled.span`
-  margin-left: 10px;
-  margin-bottom: 5px;
-  font-size: 1.5vw;
+const InfoBottomContainer = styled.div`
+  display: flex;
+  height: 30px;
 `;
 
-const Tag = styled.span``;
+const Span = styled.span.attrs(props => ({
+  role: 'img',
+}))`
+  display: flex;
+  font-size: 22px;
+  color: #000000;
+  word-break: keep-all;
+  align-items: center;
+  width: 100%;
+  .expert {
+    font-size: 1.2vw;
+    font-weight: bold;
+  }
+  .title {
+    margin-right: 10px;
+    font-size: 1.5vw;
+  }
+  .price {
+    font-size: 1.2vw;
+    opacity: 0.5;
+    flex: 1;
+    margin-right: 5px;
+    text-decoration: line-through;
+  }
+  .salePercentage {
+    font-size: 1.2vw;
+    color: #465fcc;
+    flex: 1;
+  }
+  .salePrice {
+    font-size: 1.8vw;
+    font-weight: bold;
+    flex: 3;
+    justify-content: flex-end;
+    margin-right: 15px;
+  }
+  .endTime {
+    width: 100%;
+    justify-content: center;
+    height: 23px;
+    margin-right: 10px;
+    border-radius: 41px;
+    background-color: #ff584d;
+    font-size: 1vw;
+    font-weight: bold;
+    max-width: 130px;
+    min-width: 65px;
+  }
+`;
 
-const LecturePreview = ({ id, title, imageUrl, date }) => (
+const LecturePreview = ({
+  id,
+  imageUrl,
+  heartUrl,
+  expert,
+  category,
+  endTime,
+  title,
+  price,
+  salePercentage,
+}) => (
   <Container>
     <ImageContainer>
-      <Image bgUrl={imageUrl} />
+      <Image src={imageUrl} />
+      <Heart src={heartUrl} />
     </ImageContainer>
-    <Title>{title.length > 18 ? `${title.substring(0, 15)}...` : title}</Title>
-    <Date>{date}</Date>
+    <InfoContainer>
+      <InfoTopContainer>
+        <Span>
+          <Span className="expert">
+            {expert} . {category}
+          </Span>
+          <Span className="endTime">
+            <span role="img" aria-label="time">
+              ⏰ {endTime}
+            </span>
+          </Span>
+        </Span>
+      </InfoTopContainer>
+      <InfoMidContainer>
+        <Span>
+          <Span className="title">{title}</Span>
+        </Span>
+      </InfoMidContainer>
+      <InfoBottomContainer>
+        <Span>
+          <Span className="price">{price}</Span>
+          <Span className="salePercentage">{salePercentage}%</Span>
+          <Span className="salePrice">
+            {(price * (100 - salePercentage)) / 100}
+          </Span>
+        </Span>
+      </InfoBottomContainer>
+    </InfoContainer>
   </Container>
 );
 
 LecturePreview.propTypes = {
   id: PropTypes.number.isRequired,
   imageUrl: PropTypes.string.isRequired,
+  heartUrl: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
+  expert: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  endTime: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  salePercentage: PropTypes.number.isRequired,
 };
 
 export default LecturePreview;
--- src/Components/PopularSlider.js
+++ src/Components/PopularSlider.js
@@ -4,6 +4,8 @@ import Slider from 'react-slick';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+import LecturePreview from 'Components/LecturePreview';
+
 const Container = styled.div`
   width: 100%;
   height: 100%;
@@ -39,100 +41,6 @@ const ArrowBtn = styled.div`
 const ArrowImg = styled.img``;
 
 const heartUrl = require('assets/Heart/Heart.png');
-
-const Heart = styled.img`
-  cursor: pointer;
-  z-index: 10;
-  position: absolute;
-  width: auto;
-  height: auto;
-  top: 0;
-  margin-top: 0.5%;
-  margin-left: 1vw;
-`;
-
-const ImageContainer = styled.div`
-  height: auto;
-`;
-
-const Image = styled.img`
-  width: 100%;
-`;
-
-const InfoTopContainer = styled.div`
-  height: 30px;
-`;
-
-const InfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 17px;
-  align-content: center;
-
-  ${InfoTopContainer} {
-    display: flex;
-  }
-`;
-
-const InfoMidContainer = styled.div`
-  display: flex;
-  height: 56px;
-`;
-
-const InfoBottomContainer = styled.div`
-  display: flex;
-  height: 30px;
-`;
-
-const Span = styled.span.attrs(props => ({
-  role: 'img',
-}))`
-  display: flex;
-  font-size: 22px;
-  color: #000000;
-  word-break: keep-all;
-  align-items: center;
-  width: 100%;
-  .expert {
-    font-size: 1.2vw;
-    font-weight: bold;
-  }
-  .title {
-    margin-right: 10px;
-    font-size: 1.5vw;
-  }
-  .price {
-    font-size: 1.2vw;
-    opacity: 0.5;
-    flex: 1;
-    margin-right: 5px;
-    text-decoration: line-through;
-  }
-  .salePercentage {
-    font-size: 1.2vw;
-    color: #465fcc;
-    flex: 1;
-  }
-  .salePrice {
-    font-size: 1.8vw;
-    font-weight: bold;
-    flex: 3;
-    justify-content: flex-end;
-    margin-right: 15px;
-  }
-  .endTime {
-    width: 100%;
-    justify-content: center;
-    height: 23px;
-    margin-right: 10px;
-    border-radius: 41px;
-    background-color: #ff584d;
-    font-size: 1vw;
-    font-weight: bold;
-    max-width: 130px;
-    min-width: 65px;
-  }
-`;
 
 export default class PopularSlider extends Component {
   constructor() {
@@ -191,38 +99,19 @@ export default class PopularSlider extends Component {
               url,
             } = item;
             return (
-              <div key={id}>
-                <ImageContainer>
-                  <Image src={url} />
-                  <Heart src={heartUrl} />
-                </ImageContainer>
-                <InfoContainer>
-                  <InfoTopContainer>
-                    <Span>
-                      <Span className="expert">
-                        {expert} . {category}
-                      </Span>
-                      <Span className="endTime">
-                        <span role="img" aria-label="time">
-                          ⏰ {endTime}
-                        </span>
-                      </Span>
-                    </Span>
-                  </InfoTopContainer>
-                  <InfoMidContainer>
-                    <Span>
-                      <Span className="title">{title}</Span>
-                    </Span>
-                  </InfoMidContainer>
-                  <InfoBottomContainer>
-                    <Span>
-                      <Span className="price">{price}</Span>
-                      <Span className="salePercentage">{salePercentage}%</Span>
-                      <Span className="salePrice">{price}</Span>
-                    </Span>
-                  </InfoBottomContainer>
-                </InfoContainer>
-              </div>
+              <LecturePreview
+                key={id}
+                id={id}
+                imageUrl={url}
+                heartUrl={heartUrl}
+                expert={expert}
+                category={category}
+                endTime={endTime}
+                title={title}
+                price={price}
+                salePercentage={salePercentage}
+                salePrice={price}
+              />
             );
           })}
         </StyledSlider>
